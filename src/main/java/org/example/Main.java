072415e1637379049a71d18ca4820d4a51bdf48b
kubaobara <siemagosciu32@gmail.com>
2023-06-05 00:12:36 +0200

    f

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("podaj wartosci 2 f.ackermanna");
+        int m = 3;
+        int n = 2;
+
+
+
+
+        System.out.println("Hello world!");
+    }
+}
