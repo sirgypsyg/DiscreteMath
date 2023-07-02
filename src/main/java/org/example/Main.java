b67b5e736db6170f4988354a6851224581e6f4cf
Jakub <siemagosciu32@gmail.com>
2023-07-02 03:29:01 +0200

    ex1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("podaj wartosci 2 f.ackermanna");
-        int m = 3;
-        int n = 2;
+        System.out.println(Ackermann.ackermann(2,2));
+
 
 
     }
